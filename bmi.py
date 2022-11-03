8426dc1c196f95433d5f3d93575b37c6018d35fa
Diyanah <ndiyanah.21@ichat.sp.edu.sg>
2022-11-03 15:03:12 +0800

    Initial console

--- bmi.py
+++ bmi.py
@@ -3,19 +3,13 @@ weight = float(input('enter weight in kg : '))
 
 def BMI(height, weight) :
     bmi = weight/(height**2) * 703
-    if (bmi < 16) :
-        return 'severely underweight', bmi
-
-    elif (bmi >= 16 and bmi < 18.5) :
+    if (bmi < 18.5) :
         return 'underweight', bmi
 
-    elif (bmi >= 18.5 and bmi < 25) :
-        return 'healthy', bmi
+    elif (bmi >= 18.5 and bmi <= 25.0) :
+        return 'underweight', bmi
 
-    elif (bmi >= 25 and bmi < 30) :
+    elif (bmi > 25.0) :
         return 'overweight', bmi
-
-    elif (bmi >= 30) :
-        return 'obese', bmi
 quote, bmi = BMI(height, weight)
 print('Your BMI is {} and yor are : {}' .format(bmi, quote))
